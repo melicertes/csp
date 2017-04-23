706fe560c5d482274624a6b7b49edd9f4532737a
Christos Panagiotou <chpanag@gmail.com>
2017-04-23 21:47:51 +0300

    dummy application routes

--- integration-layer-dsl-api/dummyserver/routes/adapterRoutes.js
+++ integration-layer-dsl-api/dummyserver/routes/adapterRoutes.js
@@ -9,6 +9,36 @@ module.exports = function (app) {
         .put(adapterService.update)
         .delete(adapterService.delete);
 
+    app.route('/adapter/viper')
+        .post(adapterService.create)
+        .put(adapterService.update)
+        .delete(adapterService.delete);
+
+    app.route('/adapter/jitsi')
+        .post(adapterService.create)
+        .put(adapterService.update)
+        .delete(adapterService.delete);
+
+    app.route('/adapter/misp')
+        .post(adapterService.create)
+        .put(adapterService.update)
+        .delete(adapterService.delete);
+
+    app.route('/adapter/intelmq')
+        .post(adapterService.create)
+        .put(adapterService.update)
+        .delete(adapterService.delete);
+
+    app.route('/adapter/taranis')
+        .post(adapterService.create)
+        .put(adapterService.update)
+        .delete(adapterService.delete);
+
+    app.route('/adapter/rt')
+        .post(adapterService.create)
+        .put(adapterService.update)
+        .delete(adapterService.delete);
+
     // app.route('/adapter/:taskId')
     //     .get(todoList.read_a_task)
     //     .put(todoList.update_a_task)
