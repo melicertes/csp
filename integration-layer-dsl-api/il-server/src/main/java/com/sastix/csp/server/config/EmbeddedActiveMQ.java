e0e454c8fc9073efb45b2c59a612875e6366a40d
iskitsas <iskitsas@gmail.com>
2017-05-11 16:56:02 +0300

    Add configurable data directory for embedded ActiveMQ

--- integration-layer-dsl-api/il-server/src/main/java/com/sastix/csp/server/config/EmbeddedActiveMQ.java
+++ integration-layer-dsl-api/il-server/src/main/java/com/sastix/csp/server/config/EmbeddedActiveMQ.java
@@ -1,6 +1,9 @@
 package com.sastix.csp.server.config;
 
+import com.sastix.csp.server.service.ErrorMessageHandler;
 import org.apache.activemq.broker.BrokerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -16,18 +19,25 @@ import javax.annotation.PostConstruct;
 */
 @Configuration
 public class EmbeddedActiveMQ {
-
+    private static final Logger LOG = LoggerFactory.getLogger(EmbeddedActiveMQ.class);
     @Value("${embedded.activemq.start}")
     Boolean embeddedActiveMQStart;
 
     @Value("${activemq.broker.url}")
     String embeddedActivemqBrokerUrl;
 
+    @Value("${embedded.activemq.data.path}")
+    String embeddedActivemqDataPath;
+
     @PostConstruct
     public void init() throws Exception {
         if(embeddedActiveMQStart != null && embeddedActiveMQStart) {
             BrokerService broker = new BrokerService();
             // configure the broker
+            if(embeddedActivemqDataPath != null) {
+                LOG.info("-- embedded.activemq.data.path = "+embeddedActivemqDataPath);
+                broker.setDataDirectory(embeddedActivemqDataPath);
+            }
             broker.addConnector(embeddedActivemqBrokerUrl);
             broker.start();
         }
