c1a8566f5af5fbc62616e2d14912884b3225a8c2
Dimitrianos Savva <dimis@di.uoa.gr>
2017-06-20 16:07:05 +0300

    SXCSP-198 downgrade bootstrap 4 to 3

---
+++ vcbridge-admin/application/src/main/java/eu/europa/csp/vcbadmin/config/MvcConfig.java
@@ -0,0 +1,81 @@
+package eu.europa.csp.vcbadmin.config;
+
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import eu.europa.csp.vcbadmin.config.formatters.DurationFormatter;
+import eu.europa.csp.vcbadmin.config.formatters.ZoneDateTimeFormatter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		// registry.addViewController("/").setViewName("dashboard");
+		// registry.addViewController("/index").setViewName("dashboard");
+		// registry.addViewController("/home").setViewName("dashboard");
+		registry.addViewController("/login").setViewName("login");
+	}
+	
+	@Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        PageableHandlerMethodArgumentResolver resolver = new PageableHandlerMethodArgumentResolver();
+        resolver.setOneIndexedParameters(true);
+        argumentResolvers.add(resolver);
+        super.addArgumentResolvers(argumentResolvers);
+    }
+
+	@Override
+	public void addFormatters(FormatterRegistry registry) {
+		// conflict with property editors
+		registry.addFormatter(new DurationFormatter("HH:mm"));
+		registry.addFormatter(new ZoneDateTimeFormatter(DateTimeFormatter.ISO_ZONED_DATE_TIME));
+	}
+
+	// Retry policy, used with exceptions in meeting scheduling
+	@Bean
+	public RetryTemplate retryTemplate() {
+		RetryTemplate retryTemplate = new RetryTemplate();
+
+		FixedBackOffPolicy fixedBackOffPolicy = new FixedBackOffPolicy();
+		fixedBackOffPolicy.setBackOffPeriod(5000l);
+		retryTemplate.setBackOffPolicy(fixedBackOffPolicy);
+
+		SimpleRetryPolicy retryPolicy = new SimpleRetryPolicy();
+		retryPolicy.setMaxAttempts(2);
+		retryTemplate.setRetryPolicy(retryPolicy);
+
+		return retryTemplate;
+	}
+	
+	@Bean
+	 public ResourceBundleMessageSource messageSource() {
+	  ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+	  source.setBasenames("i18n/messages");  // name of the resource bundle 
+	  source.setUseCodeAsDefaultMessage(true);
+	  return source;
+	 }
+	
+	@Bean
+    public TaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(50);
+        return executor;
+    }
+}
