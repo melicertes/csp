fcab2e5408e356ba7c1b629584b3d68df6503675
Christos Panagiotou <chpanag@gmail.com>
2017-07-14 14:55:26 +0300

    rename anonymization module, remove anonymization-project

--- anonymization-service/src/main/java/com/intrasoft/anonymization/repository/IntegrationAnonDataRepository.java
+++
@@ -1,9 +0,0 @@
-package com.intrasoft.anonymization.repository;
-
-import com.intrasoft.anonymization.model.IntegrationAnonData;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IntegrationAnonDataRepository extends JpaRepository <IntegrationAnonData, Long>{
-}
--- anonymization-service/src/main/resources/static/assets/js/pages/dashboard.js
+++
@@ -1,9 +0,0 @@
-function queryParams() {
-    return {
-        //type: ['name', 'domain'],
-        sort: 'ts',
-        direction: 'asc',
-        per_page: 10,
-        page: 1
-    };
-}
