d82ecc83a5176c33cd67cbc5371f34a72ddc3c0d
Dimitris Karadimas <dimitris@karadimas.org>
2017-07-26 21:56:02 +0300

    Configuration Service Exceptions refactor.

---
+++ configuration-service/app/src/main/java/com/intrasoft/csp/ccs/domain/RestErrorDTO.java
@@ -0,0 +1,45 @@
+package com.intrasoft.csp.ccs.domain;
+
+
+public class RestErrorDTO {
+
+    private Integer responseCode;
+    private String responseText;
+    private String responseException;
+
+    public RestErrorDTO() {}
+
+
+    public Integer getResponseCode() {
+        return responseCode;
+    }
+
+    public void setResponseCode(Integer responseCode) {
+        this.responseCode = responseCode;
+    }
+
+    public String getResponseText() {
+        return responseText;
+    }
+
+    public void setResponseText(String responseText) {
+        this.responseText = responseText;
+    }
+
+    public String getResponseException() {
+        return responseException;
+    }
+
+    public void setResponseException(String responseException) {
+        this.responseException = responseException;
+    }
+
+    @Override
+    public String toString() {
+        return "RestErrorDTO{" +
+                "responseCode=" + responseCode +
+                ", responseText='" + responseText + '\'' +
+                ", responseException='" + responseException + '\'' +
+                '}';
+    }
+}
