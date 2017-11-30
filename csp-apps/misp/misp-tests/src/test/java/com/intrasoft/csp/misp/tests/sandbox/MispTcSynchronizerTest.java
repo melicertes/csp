a3226cfed01fa8469606d857733df2d1500b7076
George Boulougaris <giorgosbg@outlook.com.gr>
2017-11-30 11:26:31 +0200

    MISP: Setting up a synchronizer unit test

---
+++ csp-apps/misp/misp-tests/src/test/java/com/intrasoft/csp/misp/tests/sandbox/MispTcSynchronizerTest.java
@@ -0,0 +1,14 @@
+package com.intrasoft.csp.misp.tests.sandbox;
+
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+
+public class MispTcSynchronizerTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MispAppClientTest.class);
+
+}
