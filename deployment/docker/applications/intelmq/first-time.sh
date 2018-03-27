806613978bbbacee958c7f512f1f033a7fcf02d7
Tomasz Kusber <tomasz.kusber@fokus.fraunhofer.de>
2018-03-27 12:11:20 +0200

    some adjustments, separation of services etc.

--- deployment/docker/applications/intelmq/first-time.sh
+++ deployment/docker/applications/intelmq/first-time.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-echo "[i] create data volumes to be used by intelmq"
+if [ ! -d /opt/csp/logs/imq ]; then
+  echo "[i] create directory for imq logs /opt/csp/logs/imq"
+  mkdir -p /opt/csp/logs/imq
+fi
+
+#echo "[i] create data volumes to be used by intelmq"
 #docker volume create SomeDatavolume
 #
