06da748b881554e6c43bb73e7d134532062ab6c4
Tomasz Kusber <tomasz.kusber@fokus.fraunhofer.de>
2018-03-27 12:11:20 +0200

    some adjustments, separation of services etc.

--- deployment/docker/applications/intelmq/last-time.sh
+++ deployment/docker/applications/intelmq/last-time.sh
@@ -7,9 +7,8 @@ docker rm csp-intelmq_emitter
 echo "[i] deleting the csp-intelmq container"
 docker rm csp-intelmq
 
-echo "[i] removing the docker volumes used by intelmq"
+#echo "[i] removing the docker volumes used by intelmq"
 #docker volume rm -f SomeDatavolume 
-
 
 echo "[i] removing the image csp-intelmq:1.0"
 docker rmi csp-intelmq:1.0
