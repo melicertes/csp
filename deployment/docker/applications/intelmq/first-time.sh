4d2dcd214d642bbb63599d710dab251ad02de018
Majid Salehi <majid.salehi.ghamsari@fokus.fraunhofer.de>
2018-04-26 09:09:10 +0000

    Update first-time.sh with creating inputout directory for botnet. and changing the permissions.

--- deployment/docker/applications/intelmq/first-time.sh
+++ deployment/docker/applications/intelmq/first-time.sh
@@ -5,6 +5,19 @@ if [ ! -d /opt/csp/logs/imq ]; then
   mkdir -p /opt/csp/logs/imq
 fi
 
+if [ ! -d /opt/csp/intelmq-fileinput ]; then
+  echo "[i] create directory for imq bot input /opt/csp/intelmq-fileinput"
+  mkdir -p /opt/csp/intelmq-fileinput
+  echo "[i] change permissions for directory /opt/csp/intelmq-fileinput"
+  chmod og+rw -R /opt/csp/intelmq-fileinput
+fi
+
+if [ ! -d /opt/csp/intelmq-fileoutput ]; then
+  echo "[i] create directory for imq bot output /opt/csp/intelmq-fileouput"
+  mkdir -p /opt/csp/intelmq-fileoutput
+  echo "[i] change permissions for directory /opt/csp/intelmq-fileouput"
+  chmod og+rw -R /opt/csp/intelmq-fileoutput
+fi
 #echo "[i] create data volumes to be used by intelmq"
 #docker volume create SomeDatavolume
 #
