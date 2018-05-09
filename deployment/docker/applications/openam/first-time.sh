b5fa5beec90139aec394bffb3209c36a47c00ac1
Majid Salehi <majid.salehi.ghamsari@fokus.fraunhofer.de>
2018-05-09 09:02:12 +0000

    Update first-time.sh for Installing OAM from scratch.

--- deployment/docker/applications/openam/first-time.sh
+++ deployment/docker/applications/openam/first-time.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-echo "[i] create data volume (OAMDatavolume)"
+echo "[i] removing the csp-oam container"
+docker rm csp-oam
 
+echo "[i] removing the docker volumes"
+docker volume rm OAMDatavolume
+
+echo "[i] create data volume (OAMDatavolume)"
 docker volume create OAMDatavolume
 
 echo "[i] done."
-
