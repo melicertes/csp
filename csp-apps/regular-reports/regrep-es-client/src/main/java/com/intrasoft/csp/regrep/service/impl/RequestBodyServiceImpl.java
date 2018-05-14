c04fe61ab268e3cddb0f9b3f7093ab3f4f96b8ed
George Boulougaris <giorgosbg@outlook.com.gr>
2018-05-14 09:04:53 +0300

    SXCSP-260: Adding json payloads as resources to fix 'file not found' errors in executable jar

--- csp-apps/regular-reports/regrep-es-client/src/main/java/com/intrasoft/csp/regrep/service/impl/RequestBodyServiceImpl.java
+++ csp-apps/regular-reports/regrep-es-client/src/main/java/com/intrasoft/csp/regrep/service/impl/RequestBodyServiceImpl.java
@@ -7,17 +7,15 @@ import com.intrasoft.csp.regrep.CspDataMappingType;
 import com.intrasoft.csp.regrep.DateMath;
 import com.intrasoft.csp.regrep.LogstashMappingType;
 import com.intrasoft.csp.regrep.service.RequestBodyService;
-import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.charset.Charset;
 
 @Service
 public class RequestBodyServiceImpl implements RequestBodyService {
@@ -27,21 +25,18 @@ public class RequestBodyServiceImpl implements RequestBodyService {
     @Autowired
     ObjectMapper objectMapper;
 
-    URL nLogsByType   = getClass().getClassLoader().getResource("json.payloads/nlogs-by-type.json");
-    URL nDocsByType   = getClass().getClassLoader().getResource("json.payloads/ndocs-by-type.json");
-    URL dailyExcLogs  = getClass().getClassLoader().getResource("json.payloads/daily-exc-logs.json");
+    String payload;
+
+    Resource nLogsByType = new ClassPathResource("json.payloads/nlogs-by-type.json");
+    Resource nDocsByType = new ClassPathResource("json.payloads/ndocs-by-type.json");
+    Resource dailyExcLogs= new ClassPathResource("json.payloads/daily-exc-logs.json");
 
     final String TIME_DIF = "-9h";
 
     @Override
     public String buildRequestBody(DateMath gte, DateMath lt, LogstashMappingType type) {
 
-        String payload = null;
-        try {
-            payload = FileUtils.readFileToString(new File(nLogsByType.toURI()), Charset.forName("UTF-8"));
-        } catch (IOException | URISyntaxException e) {
-            LOG.error(e.getMessage());
-        }
+        payload = getResourceAsString(nLogsByType);
 
         JsonNode jsonNode = null;
         try {
@@ -62,12 +57,8 @@ public class RequestBodyServiceImpl implements RequestBodyService {
 
     @Override
     public String buildRequestBody(DateMath gte, DateMath lt, CspDataMappingType type) {
-        String payload = null;
-        try {
-            payload = FileUtils.readFileToString(new File(nDocsByType.toURI()), Charset.forName("UTF-8"));
-        } catch (IOException | URISyntaxException e) {
-            LOG.error(e.getMessage());
-        }
+
+        payload = getResourceAsString(nDocsByType);
 
         JsonNode jsonNode = null;
         try {
@@ -88,12 +79,8 @@ public class RequestBodyServiceImpl implements RequestBodyService {
 
     @Override
     public String buildRequestBodyForLogs(DateMath gte, DateMath lt, LogstashMappingType type) {
-        String payload = null;
-        try {
-            payload = FileUtils.readFileToString(new File(dailyExcLogs.toURI()), Charset.forName("UTF-8"));
-        } catch (IOException | URISyntaxException e) {
-            LOG.error(e.getMessage());
-        }
+
+        payload = getResourceAsString(dailyExcLogs);
 
         JsonNode jsonNode = null;
         try {
@@ -113,5 +100,15 @@ public class RequestBodyServiceImpl implements RequestBodyService {
         return payload;
     }
 
+    private String getResourceAsString(Resource resource) {
+
+        try {
+            return IOUtils.toString(resource.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
 
 }
