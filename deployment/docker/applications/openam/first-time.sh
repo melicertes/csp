055e6c3cf6970eb17cf84fef3b1beab15b86e466
Tomasz Kusber <tomasz.kusber@fokus.fraunhofer.de>
2018-05-15 11:41:17 +0200

    special treatment for version 3.6.x

--- deployment/docker/applications/openam/first-time.sh
+++ deployment/docker/applications/openam/first-time.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-echo "[i] removing the csp-oam container"
-docker rm csp-oam
-
-echo "[i] removing the docker volumes"
-docker volume rm OAMDatavolume
-
 echo "[i] create data volume (OAMDatavolume)"
 docker volume create OAMDatavolume
 
