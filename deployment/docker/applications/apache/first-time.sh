94720a9d4eb6f70ed791af888c2df5899e860334
akribopo <akribopo@gmail.com>
2018-05-17 01:59:06 +0300

    Creating installer_net in case is missing

--- deployment/docker/applications/apache/first-time.sh
+++ deployment/docker/applications/apache/first-time.sh
@@ -3,3 +3,8 @@
 docker volume create TCDatavolume
 docker volume create WebAgentsDatavolume
 
+docker network  ls --format '{{.Name}}' | grep installer_net  > /dev/null
+if [[ "$?" -gt 0 ]] ; then
+    echo "Creating docker network installer_net"
+    echo $(docker network create installer_net)
+fi
