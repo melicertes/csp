cb2bc770b19b27f1c390a5fe2d58549b44673902
Tomasz Kusber <tomasz.kusber@fokus.fraunhofer.de>
2018-05-17 20:46:45 +0200

    two new volumes added OAMOptvolume and OAMWebappsvolume

--- deployment/docker/applications/openam/last-time.sh
+++ deployment/docker/applications/openam/last-time.sh
@@ -6,6 +6,10 @@ docker rm csp-oam
 echo "[i] removing the docker volumes"
 docker volume rm OAMDatavolume 
 
+docker volume rm OAMOptvolume
+
+docker volume rm OAMWebappsvolume
+
 echo "[i] removing the image csp-openam:1.0"
 docker rmi csp-openam:1.0
 
