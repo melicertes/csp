e16f0f2982403a40a6605fdeb869c22c0d21b71d
George Boulougaris <giorgosbg@outlook.com.gr>
2018-06-13 09:04:45 +0300

    SXCSP-505: Rectifying an event by parsing its main attributes distribution levels plus test.

--- csp-apps/misp/misp-tests/src/test/java/com/intrasoft/csp/misp/tests/sandbox/DistributionPolicyRectifierTest.java
+++ csp-apps/misp/misp-tests/src/test/java/com/intrasoft/csp/misp/tests/sandbox/DistributionPolicyRectifierTest.java
@@ -2,6 +2,7 @@ package com.intrasoft.csp.misp.tests.sandbox;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.intrasoft.csp.misp.service.DistributionPolicyRectifier;
 import com.intrasoft.csp.misp.service.impl.DistributionPolicyRectifierImpl;
 import org.apache.commons.io.IOUtils;
@@ -17,7 +18,9 @@ import org.springframework.core.io.Resource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.io.IOException;
+
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = {DistributionPolicyRectifierImpl.class, ObjectMapper.class})
@@ -41,12 +44,42 @@ public class DistributionPolicyRectifierTest {
         jsonNode = getResourceAsJsonNode(zmqEventSampleA);
     }
 
-    // Attribute with stricter sharing policy than the event should be deleted
+    // The attribute with stricter sharing policy than the event should be deleted
+    // The attribute with the same sharing policy with the event should not be deleted
     @Test
-    public void rectifyEventFirstTest() {
+    public void rectifyEventShouldDeleteAttributeWithLowerDistributionLevelTest() {
+
+        // Get all main the attributes of the event into an array and get hold of its size
+        ArrayNode attributesArray = (ArrayNode) jsonNode.path("Event").path("Attribute");
+        int initialNumberOfAttributes = attributesArray.size();
+
         distributionPolicyRectifier.rectifyEvent(jsonNode);
-        // TODO test
+
+        // Array's size should be its initial size-1 after invoking the method in test
+        attributesArray = (ArrayNode) jsonNode.path("Event").path("Attribute");
+        assertTrue( attributesArray.size() == initialNumberOfAttributes-1);
+
+
+        // Assert that attribute with id 11 is deleted (distribution=3) and attrib with id 1 is not (distrib=4)
+        boolean idAfound = false;
+        boolean idBfound = false;
+        for (JsonNode attribute : attributesArray) {
+            if (attribute.get("id").textValue().equals("11"))
+              idAfound = true;
+            if (attribute.get("id").textValue().equals("1"))
+                idBfound = true;
+        }
+        assertFalse(idAfound);
+        assertTrue(idBfound);
+
     }
+
+    @Test
+    public void rectifyEventSecondTest() {
+        distributionPolicyRectifier.rectifyEvent(jsonNode);
+
+    }
+
 
     private JsonNode getResourceAsJsonNode(Resource resource) {
         JsonNode jsonNode = null;
