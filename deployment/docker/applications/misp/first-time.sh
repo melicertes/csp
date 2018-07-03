dea298ee29e18888833022abcdb1c88871b94b24
akribopo <akribopo@gmail.com>
2018-07-03 11:17:10 +0300

    Fixing MISP Upgrade

--- deployment/docker/applications/misp/first-time.sh
+++ deployment/docker/applications/misp/first-time.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/env bash
 
-docker volume create MISPDatavolume
+docker volume create MISPConfigVolume
+docker volume create MISPGnuVolume
 docker volume create MISPAdapterDatavolume
 docker volume create MYSQLDatavolume
 docker volume create MISPSharedDatavolume
 docker volume create MISPStateDatavolume
+
+
+docker ps -a --format '{{.Names}}' | grep csp-misp  > /dev/null
+if [[ "$?" -eq 0 ]] ; then
+    echo ">> Found old MISP Installation"
+    echo ">> Searching MISPDatavolume Volume"
+    docker volume  ls --format '{{.Name}}' | grep MISPDatavolume  > /dev/null
+    if [[ "$?" -eq 0 ]] ; then
+        echo ">>  Found Docker volume MISPDatavolume"
+        docker run -d --rm -v MISPDatavolume:/old -v MISPConfigVolume:/var/www/MISP/app/Config frolvlad/alpine-oraclejdk8:slim sh -c "cp /old/app/Config/* /var/www/MISP/app/Config/"
+        docker run -d --rm -v MISPDatavolume:/old -v MISPGnuVolume:/var/www/MISP/.gnupg frolvlad/alpine-oraclejdk8:slim sh -c "cp /old/.gnupg/* /var/www/MISP/.gnupg/"
+    fi
+    docker rm csp-misp
+    docker MISPDatavolume
+fi
 
 mkdir -p /opt/csp/logs_misp/
 chown -R 33.33 /opt/csp/logs_misp/
