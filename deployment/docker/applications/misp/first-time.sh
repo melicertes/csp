3906744f9b7a342e52d2cf0fbe89c613ecf6f5ab
akribopo <akribopo@gmail.com>
2018-07-03 11:27:49 +0300

    Fixing MISP Upgrade

--- deployment/docker/applications/misp/first-time.sh
+++ deployment/docker/applications/misp/first-time.sh
@@ -8,17 +8,22 @@ docker volume create MISPSharedDatavolume
 docker volume create MISPStateDatavolume
 
 
+docker volume  ls --format '{{.Name}}' | grep MISPDatavolume  > /dev/null
+if [[ "$?" -eq 0 ]] ; then
+    echo ">>  Found Docker volume MISPDatavolume"
+    docker run -d --rm -v MISPDatavolume:/old -v MISPConfigVolume:/var/www/MISP/app/Config frolvlad/alpine-oraclejdk8:slim sh -c "cp /old/app/Config/* /var/www/MISP/app/Config/"
+    docker run -d --rm -v MISPDatavolume:/old -v MISPGnuVolume:/var/www/MISP/.gnupg frolvlad/alpine-oraclejdk8:slim sh -c "cp /old/.gnupg/* /var/www/MISP/.gnupg/"
+fi
+
 docker ps -a --format '{{.Names}}' | grep csp-misp  > /dev/null
 if [[ "$?" -eq 0 ]] ; then
     echo ">> Found old MISP Installation"
-    echo ">> Searching MISPDatavolume Volume"
-    docker volume  ls --format '{{.Name}}' | grep MISPDatavolume  > /dev/null
-    if [[ "$?" -eq 0 ]] ; then
-        echo ">>  Found Docker volume MISPDatavolume"
-        docker run -d --rm -v MISPDatavolume:/old -v MISPConfigVolume:/var/www/MISP/app/Config frolvlad/alpine-oraclejdk8:slim sh -c "cp /old/app/Config/* /var/www/MISP/app/Config/"
-        docker run -d --rm -v MISPDatavolume:/old -v MISPGnuVolume:/var/www/MISP/.gnupg frolvlad/alpine-oraclejdk8:slim sh -c "cp /old/.gnupg/* /var/www/MISP/.gnupg/"
-    fi
     docker rm csp-misp
+fi
+
+docker volume  ls --format '{{.Name}}' | grep MISPDatavolume  > /dev/null
+if [[ "$?" -eq 0 ]] ; then
+    echo ">>  Found old Docker volume MISPDatavolume"
     docker volume rm MISPDatavolume
 fi
 
