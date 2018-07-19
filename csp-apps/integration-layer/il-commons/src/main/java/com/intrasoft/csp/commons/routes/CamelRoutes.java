28e8cd0e2d133db4fd23495e87c932d9a218ec3e
iskitsas <iskitsas@gmail.com>
2018-07-19 17:36:14 +0300

    SXCSP-456: IL platform has been configured to be async non blocking, so messages towards inactive CSPs will not affect/block messages towards active ones.

--- csp-apps/integration-layer/il-commons/src/main/java/com/intrasoft/csp/commons/routes/CamelRoutes.java
+++ csp-apps/integration-layer/il-commons/src/main/java/com/intrasoft/csp/commons/routes/CamelRoutes.java
@@ -7,7 +7,8 @@ import java.util.function.Function;
  * Created by iskitsas on 4/7/17.
  */
 public interface CamelRoutes {
-    String DIRECT ="direct";
+    String VM ="vm";
+    String DIRECT ="vm";
     String ACTIVEMQ ="activemq";
     String ACTIVEMQ_TX ="activemqtx";
     String MOCK_PREFIX ="mock";
@@ -17,20 +18,10 @@ public interface CamelRoutes {
     String EDCL ="edcl";
     String DDL ="ddl";
     String TC ="tc";
-    String TCT ="tct";
     String ECSP ="ecsp";
     String APP ="app";
     String ELASTIC ="elastic";
 
     String ORIGIN_ENDPOINT ="originEndpoint";
     String ERROR ="error";
-//String DSL ="direct:dsl";
-//    String DCL ="direct:dcl";
-//    String EDCL ="direct:edcl";
-//    String DDL ="direct:ddl";
-//    String TC ="direct:tc";
-//    String TCT ="direct:tct";
-//    String ECSP ="direct:ecsp";
-//    String APP ="direct:app";
-//    String ELASTIC ="direct:elastic";
 }
