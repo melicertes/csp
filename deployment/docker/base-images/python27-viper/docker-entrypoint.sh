0e90a91adadec1380a699f25deadf6d16f325566
Christos Panagiotou <chpanag@gmail.com>
2018-07-22 02:10:01 +0300

    viper run server from entrypoint script

--- deployment/docker/base-images/python27-viper/docker-entrypoint.sh
+++ deployment/docker/base-images/python27-viper/docker-entrypoint.sh
@@ -8,4 +8,5 @@ done
 sed -i "s@misp_url =.*@misp_url = $(echo "$MISP_LOCAL_DOMAIN")@g" /home/viper/viper/viper.conf
 sed -i "s/misp_key =.*/misp_key = $(cat /run/secrets/authkey)/g" /home/viper/viper/viper.conf
 
+python3 viper-web -H 0.0.0.0 -p 8083
 
