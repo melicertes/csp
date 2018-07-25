49b3ca82f88433ce2177effbe233cf5cb90399f2
majidsalehighamsari <majid.salehi.ghamsari@fokus.fraunhofer.de>
2018-07-25 14:27:46 +0200

    minor changes for apache start

--- deployment/docker/applications/intelmq/scripts/run.sh
+++ deployment/docker/applications/intelmq/scripts/run.sh
@@ -4,10 +4,13 @@ set -e
 
 echo "[i] starting the intlemq pipes ..."
 sudo chmod -R ugo+w /opt/intelmq/var/log
-sudo -u intelmq /usr/bin/intelmqctl start
-
-rm -f /run/apache2/httpd.pid
+sudo -u intelmq /usr/local/bin/intelmqctl start
+echo "[i] starting the intlemq pipes done"
 
 echo "[i] starting the apache server for intelmq manager ..."
-exec /usr/sbin/httpd -DFOREGROUND
+if [ -f "$APACHE_PID_FILE" ]; then
+    sudo rm "$APACHE_PID_FILE"
+fi
+exec /usr/sbin/apache2ctl -D FOREGROUND
+
 
