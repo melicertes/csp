6d93eb7350dccd477cb56608836e67ca5dc58d2b
majidsalehighamsari <majid.salehi.ghamsari@fokus.fraunhofer.de>
2019-01-30 14:07:24 +0100

    less debug logging (review)

--- deployment/docker/applications/intelmq/csp-config/cve/parser_cve_server.py
+++ deployment/docker/applications/intelmq/csp-config/cve/parser_cve_server.py
@@ -13,24 +13,24 @@ class CveserverParserBot(Bot):
 
     def process(self):
         report = self.receive_message()
-        self.logger.debug("CveserverParserBot 2.0 starts....")
+        self.logger.debug("CveserverParserBot 2.1 starts....")
         raw_report = utils.base64_decode(report.get('raw'))
         report_json = json.loads(raw_report)
-        self.logger.debug("CveserverParserBot report_json %s", report_json)
+        #self.logger.debug("CveserverParserBot report_json %s", report_json)
         for item in report_json:
-            self.logger.debug("CveserverParserBot item %s", item)
+            #self.logger.debug("CveserverParserBot item %s", item)
             event = self.new_event(report)
             # add some other remaining fields as extra
             additional = {}            
             misp_event = {"info":"","threat_level_id":"1","attribute_count":"6", "distribution": "1","Object":[{"name":"vulnerability","meta-category":"network","description":"","Attribute":[{"object_relation":"id","type":"vulnerability","value":""},{"type":"text","object_relation":"text","value":""},{"type":"text","object_relation":"summary","value":""},{"type":"text","object_relation":"vulnerable_configuration","value":""},{"type":"link","category":"External analysis","object_relation":"references","value":""},{"type":"text","value":"Published"}]}],"Tag":[{"name":"vulnerability"}]}
 
             if 'id' in item:
-                self.logger.debug("CveserverParserBot item id %s", item['id'])               
+                #self.logger.debug("CveserverParserBot item id %s", item['id'])
                 additional['id'] = item['id']   
                 misp_event["Object"][0]["Attribute"][0]["value"] = item['id']
                 event.add('event_description.target', item['id'])          
             if 'cvss' in item:
-                self.logger.debug("CveserverParserBot item cvss %s", item['cvss'])
+                #self.logger.debug("CveserverParserBot item cvss %s", item['cvss'])
                 additional['cvss'] = item['cvss']     
             if 'cwe' in item:
                 additional['cwe'] = item['cwe']     
@@ -52,23 +52,23 @@ class CveserverParserBot(Bot):
             #vulnerable_configuration 
             if 'vulnerable_configuration' in item:
                 if len(item['vulnerable_configuration']) != 0:
-                    self.logger.debug("CveserverParserBot item vulnerable_configuration %s", item['vulnerable_configuration'][0])                    
+                    #self.logger.debug("CveserverParserBot item vulnerable_configuration %s", item['vulnerable_configuration'][0])                    
                     misp_event["Object"][0]["Attribute"][3]["value"] = item['vulnerable_configuration'][0]
 
             #references 
             if 'references' in item:
                 if len(item['references']) != 0:
-                    self.logger.debug("CveserverParserBot item reference %s", item['references'][0])
+                    #self.logger.debug("CveserverParserBot item reference %s", item['references'][0])
                     event.add('event_description.url', item['references'][0])
                     misp_event["Object"][0]["Attribute"][4]["value"] = item['references'][0]
 
                                                  
             #misp_event["Event"]["Attribute"][0]["timestamp"] = misp_event["Event"]["timestamp"]
-            event.add("extra", additional)                        
+            event.add("extra", additional)
             #event.add("raw", json.dumps(item, sort_keys=True))  # sorting for undefined order
             event.add("raw", json.dumps(misp_event, sort_keys=True))  # sorting for undefined order
             self.send_message(event)
-		self.logger.debug("CveserverParserBot 2.0 end.")
+        self.logger.debug("CveserverParserBot 2.1 end.")
         self.acknowledge_message()
 
 
