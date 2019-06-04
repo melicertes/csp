112fb1ae375a4a1c0ca556fd527e44b91c3c0d72
Thanos Angelatos <thanos.angelatos@persado.com>
2019-06-04 18:37:08 +0300

    Updates in vcb

--- deployment/docker/applications/vcb/build.sh
+++ deployment/docker/applications/vcb/build.sh
@@ -1,2 +1,6 @@
+cp ../../../../csp-apps/vcbridge/vcb-admin/target/vcb-admin-*-SNAPSHOT-exec.jar ./vcb-admin-exec.jar
+cp ../../../../csp-apps/vcbridge/vcb-teleconf/target/vcb-teleconf-*-SNAPSHOT-exec.jar ./vcb-teleconf-exec.jar
+
+
 docker build -t csp-jitsimeet:1.0 -f Dockerfile.jitsimeet .
 
