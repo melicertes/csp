c02e20a91bce037affa6fe44e0cb63882b200acd
Thanos Angelatos <thanos.angelatos@persado.com>
2019-06-20 00:40:56 +0300

    Added init code for 4.2.x

--- deployment/docker/applications/activemq/first-time.sh
+++ deployment/docker/applications/activemq/first-time.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+CHKPOINT42="/etc/.amq.42"
+if [ -f "$CHKPOINT42" ]; then
+  echo "[x] initialised for 4.2.x"	
+else
+  docker volume rm AMQDatavolume
+  echo "[x] 4.2.x init complete"
+fi
+
 docker volume create AMQDatavolume
 docker volume create AMQConfigVolume
 
