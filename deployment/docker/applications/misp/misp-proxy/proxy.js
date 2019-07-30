48fe8cd60733bc935533e3c16dcef31ab0fcb3e8
Christos Panagiotou <chpanag@gmail.com>
2019-07-30 17:11:32 +0300

    handle missing authorization header

--- deployment/docker/applications/misp/misp-proxy/proxy.js
+++ deployment/docker/applications/misp/misp-proxy/proxy.js
@@ -2,6 +2,8 @@ var express  = require('express');
 var app      = express();
 var httpProxy = require('http-proxy');
 var apiProxy = httpProxy.createProxyServer();
+var fs = require("fs");
+
 
 const API_PORT = process.env.API_PORT || 800;
 
@@ -9,23 +11,40 @@ var api_target = 'http://csp-misp:' + API_PORT;
 
 app.all("/*", function(req, res) {
 
-    console.log(req.headers)
+    console.log(req.headers);
 
     if (req.headers['accept'] === 'application/json') {
 
-        console.log('redirecting to: ' + api_target);
-
-        apiProxy.web(req, res, {
-            target: api_target,
-            secure: false,
-            changeOrigin: true,
-        }, function (e) {
-            console.log(e.message);
-            return res.status(500).send({
+        if (!req.headers['authorization']){
+            return res.status(400).send({
                 error: true,
-                message: e.message
+                message: 'Authorization header missing'
             });
-        });
+        } else {
+            fs.readFile("/run/secrets/authkey", function(err, data) {
+
+                if (data.toString().replace(/\n$/, '') != req.headers['authorization']){
+                    return res.status(403).send({
+                        error: true,
+                        message: 'not authorized'
+                    });
+                }
+
+                console.log('redirecting to: ' + api_target);
+
+                apiProxy.web(req, res, {
+                    target: api_target,
+                    secure: false,
+                    changeOrigin: true,
+                }, function (e) {
+                    console.log(e.message);
+                    return res.status(500).send({
+                        error: true,
+                        message: e.message
+                    });
+                });
+            });
+        }
     } else {
         console.log('Operation not permitted for ' + req.headers['accept']);
         return res.status(400).send({
@@ -33,7 +52,7 @@ app.all("/*", function(req, res) {
             message: 'cannot serve request'
         });
     }
-
 });
 
 app.listen(3000);
+
