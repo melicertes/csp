105a54fbfd4057052c0f6a834af404ce43350f44
Thanos <thanosa75@users.noreply.github.com>
2019-11-08 11:52:23 +0200

    make path relatvie

--- deployment/docker/applications/trustcircles/build.sh
+++ deployment/docker/applications/trustcircles/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-rsync -va /home/csp/github/csp/csp-apps/tc/tc/ ./tc
-
+#rsync -va /home/csp/github/csp/csp-apps/tc/tc/ ./tc
+# make relative
+rsync -va ../../../../csp-apps/tc/tc/ ./tc
+echo "completed"
